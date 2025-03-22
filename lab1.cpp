4387d7146aca909f6b5565628c953f1d5aae6136
lidi-1 <lidiyagirma75@gmail.com>
2025-03-22 23:03:01 +0300

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,34 @@
+// overloading functions
+#include<iostream>
+using namespace std;
+int operate (2,3)
+return(2*3)
+double int 2,double int 3
+return(int 2+int 3)
+
+
+
+template <typename sos>
+sos add (sos 7, sos 3)
+return(7*3)
+
+
+
+Struct Product
+{product 1
+int id, A1
+int no, 120 
+
+
+
+
+}
+
+int operate (int a, int b)
+
+{return (a*b);
+    int a, 12
+    int b, 3
+}
+double operate (double a, double b)
+{return(a+b)}
